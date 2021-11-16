d99ae0a2eea149b1f70759c290b8990592a288f5
Suchandra Thapa <suchandra@gmail.com>
2021-11-16 12:09:04 -1000

    Fix build errors

--- resources/build_rpm.sh
+++ resources/build_rpm.sh
@@ -12,7 +12,7 @@ PKG_NAME="slate-api-server-0.1.0"
 PKG_SHORT_NAME="slate-api-server"
 GIT_REPO="https://github.com/slateci/slate-api-server.git"
 # might be able to use a later sdk version but better not to risk it
-AWS_SDK_LOCATION="https://codeload.github.com/aws/aws-sdk-cpp/tar.gz/refs/tags/1.5.25"                                                                     
+# AWS_SDK_LOCATION="https://codeload.github.com/aws/aws-sdk-cpp/tar.gz/refs/tags/1.5.25"                                                                     
 #AWS_SDK_LOCATION="https://github.com/aws/aws-sdk-cpp/archive/refs/tags/1.5.25.tar.gz" 
 if [ -z "$1" ];
 then
@@ -59,9 +59,8 @@ tar czf "${WORKDIR}/SOURCES/${PKG_NAME}.tar.gz" -C "${WORKDIR}/SOURCES"  --exclu
 cur_dir=`pwd`
 cd ${WORKDIR}/SOURCES/
 git clone -b 1.9.145 https://github.com/aws/aws-sdk-cpp.git --recursive
-cd aws-sdk-cpp
 mv aws-sdk-cpp aws-sdk-cpp-1.9.145
-tar cvzf aws-sdk-cpp-1.5.25.tar.gz aws-sdk-cpp-1.5.25
+tar cvzf aws-sdk-cpp-1.9.145.tar.gz aws-sdk-cpp-1.9.145
 cd $cur_dir
 
 # Exceuting rpmbuild
